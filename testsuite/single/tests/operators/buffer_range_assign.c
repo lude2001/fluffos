ae8a687fa34a442c0891c6109ba342e734e84277
卢德湘 <1185804689@qq.com>
2026-07-13 19:50:30 +0800

    test: cover buffer range assignment backport

--- testsuite/single/tests/operators/buffer_range_assign.c
+++ testsuite/single/tests/operators/buffer_range_assign.c
@@ -28,4 +28,42 @@ void do_tests() {
   ASSERT_EQ(8, b[2]);
   ASSERT_EQ(9, b[3]);
   ASSERT_EQ(4, b[4]);
+
+  {
+    buffer rhs = allocate_buffer(3);
+
+    b = allocate_buffer(4);
+    b[0] = 'a';
+    b[1] = 'b';
+    b[2] = 'c';
+    b[3] = 'd';
+    rhs[0] = 'X';
+    rhs[1] = 'Y';
+    rhs[2] = 'Z';
+    b[1..1] = rhs;
+
+    ASSERT_EQ(6, sizeof(b));
+    ASSERT_EQ('a', b[0]);
+    ASSERT_EQ('X', b[1]);
+    ASSERT_EQ('Y', b[2]);
+    ASSERT_EQ('Z', b[3]);
+    ASSERT_EQ('c', b[4]);
+    ASSERT_EQ('d', b[5]);
+  }
+
+  {
+    buffer rhs = allocate_buffer(1);
+
+    b = allocate_buffer(4);
+    b[0] = 'a';
+    b[1] = 'b';
+    b[2] = 'c';
+    b[3] = 'd';
+    rhs[0] = 'Q';
+    b[1..3] = rhs;
+
+    ASSERT_EQ(2, sizeof(b));
+    ASSERT_EQ('a', b[0]);
+    ASSERT_EQ('Q', b[1]);
+  }
 }
